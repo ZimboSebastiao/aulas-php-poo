f156391baa5512ea31b2a7062c5d5303f774b6ab
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-21 11:24:41 -0300

    Criação da classe cliente

---
+++ src/Cliente.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Cliente{
+    // Propriedades (ou atributos)
+    public string $nome;
+    public string $email;
+    public array $telefones;
+    public string $senha;
+
+}
+
+
+
+?>
