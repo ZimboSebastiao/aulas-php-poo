01d11bf07f1d6777860fb0f531748f1ad0cbd172
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-22 09:13:13 -0300

    Método construtor

--- index.php
+++ index.php
@@ -11,16 +11,19 @@
     <h2>Assuntos abordados:</h2>
 
     <ul>
-        <li>Acesso direito às propriedades</li>
-        <li>Atribuição e leitura de dados</li>
-        <li>Chamada de método</li>
-        <li>Uso da palavra chave <code>this</code> para acessar recursos dentro da própria classe</li>
+        <li>Método construtor com atribuição obrigatória de valores aos parâmetros/propriedades do objeto</li>
+        <li>Uso do <code>$this</code> para acesso às propriedades dentro do objeto</li>
     </ul>
 
 <?php 
 require_once "src/Cliente.php";
+// Chamando o construtor através da classe Cliente
+$clienteA = new Cliente("Fulano", "fulano@gmail.com", ["11-6845-8678"]);
+$clienteB = new Cliente("Ozzy", "ozzy@gmail.com", ["11-6894-8678"]);
+
+//$clienteA->telefones = ["11-5464-4667"];
 
 ?>
-
+<pre><?=var_dump($clienteA, $clienteB)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -9,10 +9,11 @@ class Cliente{
 
     // Método Construtor - Permite configurar a forma de inicialização do objeto, exigindo a atribuição de dados no momento de criar objeto/instância. O construtor é sempre chamado de forma automática assim que o objeto é criado.
 
-    public function __construct(string $nome, string $email) {
+    public function __construct(string $nome, string $email, array $telefones) {
         // Atribuindo os valores dos parâmetros às propriedades da classe Cliente. Usamos o $this para acessar as propriedades dessa classe
         $this->nome = $nome;
         $this->email = $email;
+        $this->telefones = $telefones;
     }
 
 }
