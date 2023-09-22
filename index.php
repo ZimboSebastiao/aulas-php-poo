c282be899b73496dbc6d80cc930d590851c94469
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-22 09:46:48 -0300

    Usando o setter para atribuir um valor à propriedade privada

--- index.php
+++ index.php
@@ -3,27 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 3</title>
+    <title>Exemplo 4</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 3</h1>
+    <h1>PHP com POO - Exemplo 4</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
 
     <ul>
-        <li>Método construtor com atribuição obrigatória de valores aos parâmetros/propriedades do objeto</li>
-        <li>Uso do <code>$this</code> para acesso às propriedades dentro do objeto</li>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibilidade(public, private, protect)</li>
     </ul>
 
 <?php 
 require_once "src/Cliente.php";
-// Chamando o construtor através da classe Cliente
-$clienteA = new Cliente("Fulano", "fulano@gmail.com", ["11-6845-8678"]);
-$clienteB = new Cliente("Ozzy", "ozzy@gmail.com", ["11-6894-8678"]);
 
-//$clienteA->telefones = ["11-5464-4667"];
+$clienteA = new Cliente;
+
+// Código abaixo dará erro pois a propriedade é privada
+//$clienteA->nome = "Zimbo";
+
+
+// Usando o setter para atribuir um valor à propriedade privada "nome"
+
+$clienteA->setNome("João Paulo")
+
 
 ?>
-<pre><?=var_dump($clienteA, $clienteB)?></pre>
+<pre><?= var_dump($clienteA) ?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -2,19 +2,17 @@
 
 class Cliente{
     // Propriedades (ou atributos)
-    public string $nome;
-    public string $email; 
-    public array $telefones;
-    public string $senha;
+    private string $nome;
+    private string $email; 
+    private string $senha;
 
-    // Método Construtor - Permite configurar a forma de inicialização do objeto, exigindo a atribuição de dados no momento de criar objeto/instância. O construtor é sempre chamado de forma automática assim que o objeto é criado.
+    // Métodos getters e setters
 
-    public function __construct(string $nome, string $email, array $telefones) {
-        // Atribuindo os valores dos parâmetros às propriedades da classe Cliente. Usamos o $this para acessar as propriedades dessa classe
+    public function setNome(string $nome):void {
         $this->nome = $nome;
-        $this->email = $email;
-        $this->telefones = $telefones;
     }
+
+
 
 }
 
