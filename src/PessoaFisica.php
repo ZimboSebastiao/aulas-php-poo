f94222e8dc76578c0c5201e695bfd5ab923e6896
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-26 09:02:49 -0300

    Getter e Setter - Idade

---
+++ src/PessoaFisica.php
@@ -0,0 +1,37 @@
+<?php 
+
+class PessoaFisica {
+    private int $idade;
+    private string $cpf; 
+
+    // Getters e Setters IDADE
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+
+    public function setIdade(int $idade): self
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    // Getters e Setters CPF
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): self
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+}
+
+
+
+?>
