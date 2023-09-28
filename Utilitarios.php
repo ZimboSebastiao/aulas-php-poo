78e50ff75f9931262f0daa8001475375111124e5
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-28 11:34:06 -0300

    Create Utilitarios.php

---
+++ Utilitarios.php
@@ -0,0 +1,19 @@
+<?php 
+
+class Utilitarios {
+
+    // Propriedade estática
+    public static string $dataAtual;
+
+    // Método estático
+    public static function obterData(){
+        //  self::propriedade
+        //Permite o acesso à propriedade estática 
+        self::$dataAtual = date("d/m/Y");
+    }
+
+}
+
+
+
+?>
