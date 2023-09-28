673a43fd2bc1591bce23f285e47bba119c0ec6ea
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-28 10:48:45 -0300

    polimofirmos

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 5</title>
+    <title>Exemplo 6</title>
     <style>
         .formart {
             text-align: center;
@@ -14,15 +14,11 @@
     </style>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 5</h1>
+    <h1>PHP com POO - Exemplo 6</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
 
     <ul>
-        <li>Herança (Super Classe e Sub Classe)</li>
-        <li>Métodos/propriedades protegidos (acessivéis apenas pela clase em que foram definidos e pelas subclasses)</li>
-        <li>Classe Abstrata: Não permite instâncias/objetos, serve apenas de modelo para subclasses</li>
-        <li>Classe Final: Não permite herança, ou seja, não aceita a criação de subclasses</li>
         
     </ul>
 <?php 
@@ -49,34 +45,6 @@
 
 
 
-<ul class="formart">
-    <h2>Cliente Pessoa Jurídica</h2>
-    <li>Nome: <?=$clientePJ->getNome()?></li>
-    <li>E-mail: <?=$clientePJ->getEmail()?></li>
-    <li>Ano: <?=$clientePJ->getAnoFundacao()?></li>
-    <li>CNPJ: <?=$clientePJ->getCnpj()?></li>
-    <li>Nome Fantasia: <?=$clientePJ->getNomeFantasia()?></li>
-</ul>
-
-
-<pre><?=var_dump($clientePF)?></pre>
-<pre><?=var_dump($clientePJ)?></pre>
-
-
-
-<?php 
-require_once "src/MEI.php";
-
-$clienteMei = new MEI;
-$clienteMei->setNome("Marina");
-$clienteMei->setAnoFundacao(2023);
-$clienteMei->setNomeFantasia("Tanaka Emprendimentos");
-$clienteMei->setAreaDeAtuacao("Ervas medicinais");
-
-
-?>
-
-<pre><?=var_dump($clienteMei)?></pre>
 
 
 
@@ -85,16 +53,5 @@ $clienteMei->setAreaDeAtuacao("Ervas medicinais");
 
 
 
-
-
-
-
-<?php
-// o erro é devido ao facto da classe Cliente ser uma class abstrata
-$clienteGenerico = new Cliente;
-
-?>
-
-<pre><?=var_dump($clienteGenerico)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,14 +1,21 @@
 <?php 
 
-// Classe Abstrata: Não pode ser instanciada, ou seja, 
-// não é possível criar objeto a partir desta classe. 
-// Ela é usada apenas como modelo para subclasses com herança
 abstract class Cliente{
     // Propriedades (ou atributos)
     private string $nome;
     private string $email; 
     private string $senha;
     private string $situacao = "À definir!";
+
+    public function exibirDados():void {
+        echo "<h2> Método exibirDados - Classe Cliente </h2>";
+
+        echo "<h3> $this->nome </h3>";// ou
+        echo "<h3>".$this->getNome() ."</h3>";
+        
+        echo "<p>Situação: $this->situacao</p>";
+
+    }
 
     // Métodos getters e setters
 
